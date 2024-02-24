5db48ed49765bce8d075837c48a4624dd205add3
kiswaafaq <kiswaafaque14@gmail.com>
2024-02-23 23:05:20 -0800

    first commit

---
+++ names.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Kiswa', 'Uzma', 'Ajia', 'Iman'];
+const message = 'Welcome to typescript classes: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ names.ts
@@ -0,0 +1,8 @@
+let names : string []= ['Kiswa', 'Uzma', 'Ajia', 'Iman'];
+
+const message: string= 'Welcome to typescript classes: '
+
+for(let i=0; i<names.length; i++){
+    
+console.log(message + names[i]);
+}
